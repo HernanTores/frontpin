2fc13d9cc774dc339d6a22cf429b360eecab5788
DESKTOP-NRNNNH8\USUARIO <hernan_tores@hotmail.com>
2021-12-03 11:41:56 +0100

    axios modified

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/',    
+    baseURL: 'http://localhost:5000/api/',    
 })
 
 const sendEmail = async (data) => {
     const response = await instance({
         method: 'post',
-        url: 'contact/create',
+        url: 'v1',
         data: data
     })
     return response
